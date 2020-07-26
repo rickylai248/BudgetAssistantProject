6a811a85cad66b7c2eb29a204aa03e7be5aa8acf
rickylai <f5x2b@ugrad.cs.ubc.ca>
2020-07-25 17:55:17 -0700

    Phase 1: Task 4 In-Progress Personal Project "Budget Assistant"

--- src/test/model/BudgetTest.java
+++ src/test/model/BudgetTest.java
@@ -10,7 +10,6 @@ class BudgetTest {
     Budget february;
     Budget march;
     Budget april;
-    Budget may;
 
     @BeforeEach
     void runBefore() {
@@ -20,38 +19,79 @@ class BudgetTest {
         january.setTotalExpenses();
         january.setBalance();
         february = new Budget("february", 1000);
+        february.setLivingExpenses(500);
+        february.setGroceries(300);
+        february.setRestaurants(0);
+        february.setTransportation(70);
+        february.setEntertainment(30);
+        february.setMiscellaneous(0);
+        february.setTotalExpenses();
+        february.setBalance();
         march = new Budget("march", 2500);
-        april = new Budget("april", 0);
-        may = new Budget("may",10000);
+        april = new Budget("april", 10000);
     }
 
     @Test
     void testGetMonth() {
         assertEquals("january", january.getMonth());
+        assertEquals("february", february.getMonth());
+        assertEquals("march", march.getMonth());
+        assertEquals("april", april.getMonth());
     }
 
     @Test
     void testGetBudget() {
-        assertEquals(0,january.getBudget());
+        assertEquals(0, january.getBudget());
+        assertEquals(1000, february.getBudget());
+        assertEquals(2500, march.getBudget());
+        assertEquals(10000, april.getBudget());
     }
 
     @Test
     void testGetLivingExpenses() {
         assertEquals(500, january.getLivingExpenses());
+        assertEquals(500, february.getLivingExpenses());
     }
 
     @Test
     void testGetGroceries() {
-        assertEquals(300,january.getGroceries());
+        assertEquals(300, january.getGroceries());
+        assertEquals(300, february.getGroceries());
+    }
+
+    @Test
+    void testGetRestaurants() {
+        assertEquals(0, january.getRestaurants());
+        assertEquals(0, february.getRestaurants());
+    }
+
+    @Test
+    void testGetTransportation() {
+        assertEquals(0, january.getTransportation());
+        assertEquals(70, february.getTransportation());
+    }
+
+    @Test
+    void testGetEntertainment() {
+        assertEquals(0, january.getEntertainment());
+        assertEquals(30, february.getEntertainment());
+    }
+
+    @Test
+    void testGetMiscellaneous() {
+        assertEquals(0, january.getMiscellaneous());
+        assertEquals(0, february.getMiscellaneous());
     }
 
     @Test
     void testGetTotalExpenses() {
-        assertEquals(800,january.getTotalExpenses());
+        assertEquals(800, january.getTotalExpenses());
+        assertEquals(900, february.getTotalExpenses());
     }
 
     @Test
     void testGetBalance() {
-        assertEquals(-800,january.getBalance());
+        assertEquals(-800, january.getBalance());
+        assertEquals(100, february.getBalance());
     }
 }
